86b72193aa689cbc254a21fa6e2b3b8801a8ddde
Pedro Freitas <pedrosidra0@gmail.com>
2018-09-30 22:55:17 -0300

    Add functional proof-of-concept measuring distance between objects

--- VC/prova_conceito/main.cpp
+++ VC/prova_conceito/main.cpp
@@ -47,10 +47,58 @@ void createTrackbars()
 
 	createTrackbar("Black Max","Result", &thresh,180);
 }
+
+bool sortLeftPoint(Point a, Point b)
+{
+	return a.x < b.x;
+}
+
+void findReference(Mat &image, Rect & output)
+{
+	vector<vector<Point> > contoursRed;
+	vector<Vec4i> hierarchyRed;
+	Mat redThresh;
+	// Separate red area
+	getRedArea(image, redThresh);
+	findContours(redThresh, contoursRed, hierarchyRed,
+			CV_RETR_TREE,
+			CV_CHAIN_APPROX_SIMPLE);
+	output = boundingRect(contoursRed[0]);
+
+}
+
+
+void findPiece(Mat &image,vector<Point> &workpiece)
+{
+	vector<vector<Point> > contoursBlack;
+	vector<Vec4i> hierarchyBlack;
+	Mat blackThresh;
+	getBlackArea(image, blackThresh);
+	findContours(blackThresh, contoursBlack, hierarchyBlack,
+			CV_RETR_TREE,
+			CV_CHAIN_APPROX_SIMPLE);
+	for (size_t i = 0; i < contoursBlack.size(); ++i)
+	{
+		vector <Point> polyApprox;
+		approxPolyDP(contoursBlack[i],
+				polyApprox,
+				8,
+				true);
+		if( polyApprox.size() ==4)
+			workpiece = polyApprox;
+	}	
+}
+Point findRelativeWorkpiecePosition(Rect &redRef, vector<Point> workpiece)
+{
+	sort (workpiece.begin(),workpiece.end(),sortLeftPoint);
+	Point brRed = redRef.br();
+	Point tlBlack = workpiece[0];
+	return tlBlack-brRed;
+}
 int main(int argc, char** argv )
 {
 	// Variables setup
-	Mat image, redThresh, blackThresh;
+	Mat image;
 
 	// Image setup
 	image = imread( argv[1], 1 );
@@ -64,37 +112,35 @@ int main(int argc, char** argv )
 	namedWindow("Result", WINDOW_AUTOSIZE );
 	createTrackbars();
 
-	vector<vector<Point> > contoursRed;
-	vector<vector<Point> > contoursBlack;
-	vector<Vec4i> hierarchyRed;
-	vector<Vec4i> hierarchyBlack;
-	// Processing
+	Rect referenceObject;
+	vector<Point> workpiece;
+
 	while(waitKey(30)!='q')
 	{		
 		image = imread( argv[1], 1 );
-		// Separate red area
-		getRedArea(image, redThresh);
-		// Separate black area
-		getBlackArea(image, blackThresh);
 
-		// Get contours in both
-		findContours(redThresh, contoursRed, hierarchyRed,
-				CV_RETR_TREE,
-				CV_CHAIN_APPROX_SIMPLE);
-		findContours(blackThresh, contoursBlack, hierarchyBlack,
-				CV_RETR_TREE,
-				CV_CHAIN_APPROX_SIMPLE);
-		// Draw contours in original image 
-		drawContours(image, contoursRed,
-					-1,Scalar(255,0,0),
-					2,8);
-		drawContours(image, contoursBlack,
-					-1,Scalar(255,255,255),
-					2,8);
+		findReference(image, referenceObject);
+		findPiece(image,workpiece);
+
+		// Height of the red area is ~5.0 and width is ~7.0
+		// So take the average cmPerPixel for more precision
+		double cmPerPixel = (5.0/referenceObject.height + 7.0/referenceObject.width)/2;
+
+
+		Point relativePosition = findRelativeWorkpiecePosition(
+				referenceObject,
+				workpiece);
+
+		rectangle(image, referenceObject, Scalar(0,0,255));
+		polylines(image,workpiece, true, Scalar(255,255,0));
+		line(image, referenceObject.br(),referenceObject.br()+relativePosition, Scalar(255,0,0));
+
+		double workPieceDistance = cmPerPixel*norm(relativePosition);
+
+		putText(image, "Distancia medida: " + to_string(workPieceDistance) + " cm" , Point (10,20), FONT_HERSHEY_SIMPLEX, 0.8, Scalar(255,255,255));
 
 		// Display results
 		imshow("Display Image", image);
-		imshow("Result", blackThresh);
 	}
 	return 0;
 }
