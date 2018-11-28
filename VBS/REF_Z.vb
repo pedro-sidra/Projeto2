53a2875ce63b3f8fc6d49e19b8a46e5a5c0431d6
mateusgiusti <43592755+mateusgiusti@users.noreply.github.com>
2018-11-28 18:36:01 -0200

    Update REF_Z.vb

--- VBS/REF_Z.vb
+++ VBS/REF_Z.vb
@@ -2,3 +2,5 @@ EspessuraProbe = +0.5
 DistApalpBase = -62.3539
 offset = 0
 Zzero = GetUserDRO(1001) + GetUserDRO(1002) +  EspessuraProbe - DistApalpBase + offset
+Code "G0 Z" & Zzero 'manda pra zero pra ver se deu boa
+Code "G92 Z0" 'zerou
