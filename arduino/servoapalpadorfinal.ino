9ad3b2a196e3622c6d9995b6ed7a3f389b9a7c81
Pedro Freitas <pedrosidra0@gmail.com>
2018-12-01 18:20:47 -0200

    Add arduino script for probe

---
+++ arduino/servoapalpadorfinal.ino
@@ -0,0 +1,43 @@
+#include<Servo.h>;
+#define SERVO 6
+
+Servo servo;
+char cmd;
+
+void setup() {
+   Serial.begin(9600);
+   servo.attach(SERVO,544,1990);
+}
+
+void loop() {
+  while(!Serial.available());
+  cmd=Serial.read();
+  switch (cmd)
+  {
+    case 's':
+      //Serial.println("oi");
+      servo.write(0);
+      delay(200);
+      break;
+    case 'd':
+      //Serial.println("tchau");
+      servo.write(180);
+      delay(200);
+      break;
+    case 'k':
+      //Serial.println("killing");
+      servo.detach();
+      delay(200);
+      break;
+    case 'r':
+      //Serial.println("revive");
+      servo.attach(SERVO,544,1990);
+      delay(200);
+      break;
+    default:
+      break;
+  }
+}
+
+  
+
