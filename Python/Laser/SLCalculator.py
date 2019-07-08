c4d8e172e233632b012c0677f03dc87e46de7d36
Pedro <pedro.freitas@ufrgs.br>
2019-07-08 08:37:23 -0300

    Usar a Matriz da Camera para Luz Estr.
    Alterar as funções de cálculo da luz estruturada para levar em conta os
    angulos de abertura da camera.
    
    Agora, nao há efeitos de borda significativos. A calibração da camera foi refeita
    para uma imagem de 640x480 que passou por um cv2.resize(im,(1280,960)).

--- Python/Laser/SLCalculator.py
+++ Python/Laser/SLCalculator.py
@@ -1,5 +1,9 @@
 import cv2
 import numpy as np
+import pdb
+
+TEST_H = 332
+import matplotlib.pyplot as plt
 
 
 def lineFromVec(vec, x, y):
@@ -20,20 +24,24 @@ class SLCalculator:
     def __init__(self, refMask,
                  ppcm,
                  tanLaserAngle,
-                 axisCenter=(0, 0)
+                 axisCenter=(0, 0),
+                 CM=None
                  ):
 
         # Init reference line params:
             # x limits (lower, upper)
             # eigenvectors (perp, parallel)
             # line function f(x) = a*x + b
+
+        self.CM = np.linalg.inv(CM)
+        self.axisCenter = axisCenter
+
         self.xlims, self.eig, (self.f, self.lineParams) = self._init_reference(
             refMask)
 
         # Save pixels per cm
         self.ppcm = ppcm
 
-        self.axisCenter = axisCenter
 
         self.tanAlfa = tanLaserAngle
 
@@ -72,9 +80,14 @@ class SLCalculator:
 
         y, x = np.nonzero(mask)
 
-        points = np.vstack((x, y)).swapaxes(0, 1)
+        pdb.set_trace()
+        points = np.vstack((x, y, np.ones_like(y)))
 
-        (mean,), eig = cv2.PCACompute(points.astype('float32'),
+        projs = TEST_H*self.CM@points
+
+        pca_points = projs[:2].swapaxes(0,1).astype('float32')
+
+        (mean,), eig = cv2.PCACompute(pca_points,
                                       mean=None)
 
         v1, v2 = eig
@@ -82,8 +95,14 @@ class SLCalculator:
         self.mean = mean
         self.eig = eig
 
+        proj_rect = (eig)@(projs[:2])
+
+        self.y_ref = np.mean(proj_rect[1])
+        self.x_ref, _= (TEST_H-(105))*eig@((self.CM@np.array([self.axisCenter[0],0,1]))[:2])
+
+
         xlims = (np.min(x), np.max(x))
-        f = self._computeParallelLine(*mean)
+        f = self._computeParallelLine(*(np.mean(points[:2], axis=1)))
 
         return xlims, eig, f
 
@@ -94,12 +113,24 @@ class SLCalculator:
         xcenter, zcenter = self.axisCenter
 
         y, x = np.nonzero(mask)
-        piece_points = np.vstack((x-xcenter, y-self.f(xcenter))).astype(np.float32)
+        piece_points = np.vstack((x, y)).astype(np.float32)
+        piece_points = np.vstack((piece_points, np.ones_like(x)))
 
-        projected_piece_points = np.linalg.inv(self.eig)@piece_points
+        # piece_points = np.swapaxes(piece_points,0,1)
 
-        projected_piece_points[1, :] *= self.heightFactor
-        projected_piece_points[0, :] /= self.ppcm
+        # pdb.set_trace()
+        projected_piece_points = self.CM@piece_points
+        projected_piece_points = projected_piece_points[:2]
+        projected_piece_points = (self.eig)@projected_piece_points
+        xh, yh = projected_piece_points
+
+        z = (TEST_H - self.tanAlfa*self.y_ref)/(1-self.tanAlfa*yh)
+
+        x = +xh*z - self.x_ref
+        h = TEST_H - z
+
+        # projected_piece_points[1, :] *= self.heightFactor
+        # projected_piece_points[0, :] /= self.ppcm
 
         if filter:
             x, heights = projected_piece_points
@@ -111,9 +142,9 @@ class SLCalculator:
 
             # x = (31.47-heights)*x/31.47
             heights -= zcenter
-            return x, heights
+            return x, 10*np.ones_like(heights)
 
-        return projected_piece_points
+        return x, h-zcenter
 
     def perpLine(self, x, y):
         return self._computePerpLine(x, y)[0]
--- Python/Laser/laser.py
+++ Python/Laser/laser.py
@@ -11,13 +11,16 @@ REF_PHOTO = "/home/estudos/Pictures/Projeto2/TesteLaser/ref.jpg"
 PIECE_PHOTO = "/home/estudos/Pictures/Projeto2/TesteLaser/inclinadoBorr.jpg"
 
 # medida de um objeto na vida real e em uma imagem
-PIXELS_PER_CM = 29
+PIXELS_PER_CM = 2*29
 
 # tangente do angulo do laser com a superficie de ref.
-TAN_LASER_ANGLE = 26.2/23
+# H -> altura da peça que o laser está incidindo
+# Y -> coordenada que é alterada quando uma peça impede o caminho do laser
+# Se quando H aumenta, y aumenta -> TAN_LASER_ANGLE negativo
+# Se quando H aumenta, y diminui -> TAN_LASER_ANGLE positivo
+TAN_LASER_ANGLE = -26.2/23
 
 
-PLOT_EACH = False
 
 def parseArgs():
     # Parse arguments
@@ -29,6 +32,7 @@ def parseArgs():
                     )
     ap.add_argument("--ppcm", type=float, help="Pixels per cm on the pictures",
                     default=PIXELS_PER_CM)
+    ap.add_argument("--plot_each", required=False, action='store_true')
     return ap.parse_args()
 
 
@@ -48,26 +52,50 @@ def meanFilter(vec, w=2):
 if __name__ == "__main__":
     args = parseArgs()
 
-    ref_im = cv2.imread(args.reference)
+    contents = np.load("../CameraHandler/camera_params.npz")
+    cameraMatrix = contents['mtx']
+    distCoeff = contents['dist']
+
+    w,  h = (1280, 960)
+    optCameraMtx, roi = cv2.getOptimalNewCameraMatrix(
+        cameraMatrix, distCoeff,
+        (w, h), 0, (w, h))
+
+    mapx, mapy = cv2.initUndistortRectifyMap(cameraMatrix, distCoeff,
+                                            None, optCameraMtx, (w,h),
+                                            5)
+
+    def get_img(path):
+        dst = cv2.imread(path)
+        dst = cv2.resize(dst,(1280,960))
+        dst = cv2.remap(dst, mapx, mapy, cv2.INTER_LINEAR)
+        x, y, w, h = roi
+        dst = dst[y:y+h, x:x+w]
+        return dst
+
+    ref_im = get_img(args.reference)
 
     scanner = PieceScanner(ppcm=PIXELS_PER_CM,
                            tanLaserAngle=TAN_LASER_ANGLE,
-                           axisCenter=(346, 7.5))
+                           axisCenter=(2*346, 75),
+                           CM=optCameraMtx)
     scanner.init_reference(ref_im)
     scanner.show_reference()
 
     for piece in args.pieces:
-        piece_im = cv2.imread(piece)
+        piece_im = get_img(piece)
 
         angle = os.path.splitext(piece)[0].split("pic")[-1]
         angle = np.pi*float(angle)/180
-        x, heights = scanner.add_view(piece_im, angle)
+        x, heights, mask = scanner.add_view(piece_im, angle, return_mask=True)
 
-        if PLOT_EACH:
+        if args.plot_each:
             fig, axs = plt.subplots(1, 2)
+            piece_im[mask!=0] = (0,0,0)
             axs[0].imshow(piece_im)
             axs[0].set_title(angle)
             axs[1].plot(x, heights, 'b+')
+            plt.tight_layout()
             plt.show()
 
     points = scanner.get_piece()
@@ -85,14 +113,13 @@ if __name__ == "__main__":
 
     for i, a in enumerate(np.linspace(-np.pi, np.pi, samples)):
 
-        in_range = np.bitwise_and(angles>= a, angles < a+dtheta)
+        in_range = np.bitwise_and(angles >= a, angles < a+dtheta)
         size = in_range.nonzero()[0].size
 
         xf = xp[in_range].sum()/size
         yf = yp[in_range].sum()/size
-        points_filter[:,i] = np.array([xf,yf])
+        points_filter[:, i] = np.array([xf, yf])
 
     plt.figure()
     plt.plot(*points_filter, 'go')
     plt.show()
-
