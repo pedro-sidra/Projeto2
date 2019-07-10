41d5c98d8a9cebcd7d063f4fdb533d2a259c08b6
clausermazuim <clauser.mc@gmail.com>
2019-07-10 02:06:33 -0300

    Commented test function call to avoid unexpected behavior while importing file in other scripts.

--- Python/FourthAxis/choosePieceRotationGUI.py
+++ Python/FourthAxis/choosePieceRotationGUI.py
@@ -169,4 +169,4 @@ def getChosenPieceRotation(piece_img_file="hex.gif"):
     return gui.current_angle
 
 
-getChosenPieceRotation(piece_img_file="hex.gif")
+# getChosenPieceRotation(piece_img_file="hex.gif")
